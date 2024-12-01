e1f0530f4d09d080527a0484052447537a49b129
loi.lethanh <lethanhloi22042001@gmail.com>
2024-12-01 17:41:50 +0700

    fix giao dien header

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -1,172 +1,246 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import './Homeheader.scss'
-import logo1 from '../../assets/logo2.png'
-import khamchuyenkhoa from '../../assets/images/icons/phongkhamchuyenkhoa.png'
-import khamnhakhoa from '../../assets/images/icons/khamnhakhoa.png'
-import khamtongquat from '../../assets/images/icons/khamtongquat.png'
-import khamtuxa from '../../assets/images/icons/khamtuxa.png'
-import suckhoetinhthan from '../../assets/images/icons/suckhoetinhthan.png'
-import dichvuxetnghiem from '../../assets/images/icons/dichvuxetnghiem.png'
-import { FormattedMessage } from 'react-intl'
-import { LANGUAGES } from '../../utils'
-import { withRouter } from 'react-router';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./Homeheader.scss";
+import logo1 from "../../assets/logo2.png";
+import khamchuyenkhoa from "../../assets/images/icons/phongkhamchuyenkhoa.png";
+import khamnhakhoa from "../../assets/images/icons/khamnhakhoa.png";
+import khamtongquat from "../../assets/images/icons/khamtongquat.png";
+import khamtuxa from "../../assets/images/icons/khamtuxa.png";
+import suckhoetinhthan from "../../assets/images/icons/suckhoetinhthan.png";
+import dichvuxetnghiem from "../../assets/images/icons/dichvuxetnghiem.png";
+import { FormattedMessage } from "react-intl";
+import { LANGUAGES } from "../../utils";
+import { withRouter } from "react-router";
 
-import { changeLanguageApp } from '../../store/actions/appActions';
-import SearchDoctorByName from '../../components/Search/SearchDoctorByName';
+import { changeLanguageApp } from "../../store/actions/appActions";
+import SearchDoctorByName from "../../components/Search/SearchDoctorByName";
 
 class HomeHeader extends Component {
+  changeLanguage = (language) => {
+    this.props.changeLanguageAppRedux(language);
+    // fire redux event(actions)
+  };
 
-    changeLanguage = (language) => {
-        this.props.changeLanguageAppRedux(language)
-        // fire redux event(actions)
+  returnToHomePage = () => {
+    if (this.props.history) {
+      this.props.history.push(`/home`);
     }
+  };
 
-    returnToHomePage = () => {
-        if (this.props.history) {
-            this.props.history.push(`/home`)
-        }
+  handleViewSupportPage = () => {
+    if (this.props.history) {
+      this.props.history.push(`/support`);
     }
+  };
 
-    handleViewSupportPage = () => {
-        if (this.props.history) {
-            this.props.history.push(`/support`)
-        }
-    }
+  render() {
+    let language = this.props.language;
 
-    render() {
-        let language = this.props.language
+    // let { handleButtonClick } = this.props
 
-        // let { handleButtonClick } = this.props
+    // console.log('check props ref: ', handleButtonClick)
 
-        // console.log('check props ref: ', handleButtonClick)
+    // handleButtonClick = () => {
+    //     if (this.props.onClick) {
+    //         this.props.onClick();
+    //     }
+    // };
 
-        // handleButtonClick = () => {
-        //     if (this.props.onClick) {
-        //         this.props.onClick();
-        //     }
-        // };
-
-        return (
-            <React.Fragment>
-                <div className='home-header-container'>
-                    <div className='home-header-content'>
-                        <div className='left-content'>
-                            <i className='fas fa-bars'></i>
-                            <img src={logo1} className='header-logo' alt=''
-                                onClick={() => this.returnToHomePage()}
-                            ></img>
-                        </div>
-                        <div className='center-content'>
-                            <a href='#section-specialty' className='child-content'>
-                                <div>
-                                    <b>
-                                        <FormattedMessage id='homeheader.speciality' />
-                                    </b>
-                                </div>
-                                <div className='subs-title'><FormattedMessage id='homeheader.searchdoctor' /></div>
-                            </a>
-                            <a href='#section-clinic' className='child-content'>
-                                <div>
-                                    <b>
-                                        <FormattedMessage id='homeheader.health-facility' />
-                                    </b>
-                                </div>
-                                <div className='subs-title'><FormattedMessage id='homeheader.select-room' /></div>
-                            </a>
-                            <a href='#section-doctor' className='child-content'>
-                                <div>
-                                    <b>
-                                        <FormattedMessage id='homeheader.doctor1' />
-                                    </b>
-                                </div>
-                                <div className='subs-title'><FormattedMessage id='homeheader.select-doctor' /></div>
-                            </a>
-                            <a href='#section-handbook' className='child-content'>
-                                <div>
-                                    <b>
-                                        <FormattedMessage id='homeheader.handbook' />
-                                    </b>
-                                </div>
-                                <div className='subs-title'><FormattedMessage id='homeheader.handbook-des' /></div>
-                            </a>
-                            <div className='child-content'>
-                                <div>
-                                    <b>
-                                        <FormattedMessage id='homeheader.medical-package' />
-                                    </b>
-                                </div>
-                                <div className='subs-title'><FormattedMessage id='homeheader.generalHC' /></div>
-                            </div>
-                        </div>
-                        <div className='right-content'>
-                            <div className='support' onClick={() => this.handleViewSupportPage()}>
-                                <i className='fa fa-question-circle'></i>
-                                <FormattedMessage id='homeheader.support' />
-                            </div>
-                            <div className='flag'>
-                                <div className={language === LANGUAGES.VI ? 'language-vi active' : 'language-vi'}><span onClick={() => this.changeLanguage(LANGUAGES.VI)}>VN</span></div>
-                                <div className={language === LANGUAGES.EN ? 'language-en active' : 'language-en'}><span onClick={() => this.changeLanguage(LANGUAGES.EN)}>EN</span></div>
-                            </div>
-                        </div>
-                    </div>
+    return (
+      <React.Fragment>
+        <div className="home-header-container">
+          <div className="home-header-content">
+            <div className="left-content">
+              <i className="fas fa-bars"></i>
+              <img
+                src={logo1}
+                className="header-logo"
+                alt=""
+                onClick={() => this.returnToHomePage()}
+              ></img>
+            </div>
+            <div className="center-content">
+              <a href="#section-specialty" className="child-content">
+                <div>
+                  <b>
+                    <FormattedMessage id="homeheader.speciality" />
+                  </b>
                 </div>
-                {this.props.isShowBanner === true &&
-                    <div className='home-header-banner'>
-                        <div className='content-up'>
-                            <div className='title-1'><FormattedMessage id='banner.title1' /></div>
-                            <div className='title-2'><FormattedMessage id='banner.title2' /></div>
-                            <SearchDoctorByName />
-                        </div>
-                        <div className='content-down'>
-                            <div className='options'>
-                                <div className='option-child'>
-                                    <div className='icon-child'><img src={khamchuyenkhoa} /></div>
-                                    <div className='text-child'><b><FormattedMessage id='banner.bannerp1' /></b></div>
-                                </div>
-                                <div className='option-child'>
-                                    <div className='icon-child'><img src={khamtuxa} /></div>
-                                    <div className='text-child'><b><FormattedMessage id='banner.bannerp2' /></b></div>
-                                </div>
-                                <div className='option-child'>
-                                    <div className='icon-child'><img src={khamtongquat} /></div>
-                                    <div className='text-child'><b><FormattedMessage id='banner.bannerp3' /></b></div>
-                                </div>
-                                <div className='option-child'>
-                                    <div className='icon-child'><img src={dichvuxetnghiem} /></div>
-                                    <div className='text-child'><b><FormattedMessage id='banner.bannerp4' /></b></div>
-                                </div>
-                                <div className='option-child'>
-                                    <div className='icon-child'><img src={suckhoetinhthan} /></div>
-                                    <div className='text-child'><b><FormattedMessage id='banner.bannerp5' /></b></div>
-                                </div>
-                                <div className='option-child'>
-                                    <div className='icon-child'><img src={khamnhakhoa} /></div>
-                                    <div className='text-child'><b><FormattedMessage id='banner.bannerp6' /></b></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                }
-
-            </React.Fragment>
-        );
-    }
-
+                <div className="subs-title">
+                  <FormattedMessage id="homeheader.searchdoctor" />
+                </div>
+              </a>
+              <a href="#section-clinic" className="child-content">
+                <div>
+                  <b>
+                    <FormattedMessage id="homeheader.health-facility" />
+                  </b>
+                </div>
+                <div className="subs-title">
+                  <FormattedMessage id="homeheader.select-room" />
+                </div>
+              </a>
+              <a href="#section-doctor" className="child-content">
+                <div>
+                  <b>
+                    <FormattedMessage id="homeheader.doctor1" />
+                  </b>
+                </div>
+                <div className="subs-title">
+                  <FormattedMessage id="homeheader.select-doctor" />
+                </div>
+              </a>
+              <a href="#section-handbook" className="child-content">
+                <div>
+                  <b>
+                    <FormattedMessage id="homeheader.handbook" />
+                  </b>
+                </div>
+                <div className="subs-title">
+                  <FormattedMessage id="homeheader.handbook-des" />
+                </div>
+              </a>
+              <div className="child-content">
+                <div>
+                  <b>
+                    <FormattedMessage id="homeheader.medical-package" />
+                  </b>
+                </div>
+                <div className="subs-title">
+                  <FormattedMessage id="homeheader.generalHC" />
+                </div>
+              </div>
+            </div>
+            <div className="right-content">
+              <div
+                className="support"
+                onClick={() => this.handleViewSupportPage()}
+              >
+                <i className="fa fa-question-circle"></i>
+                <FormattedMessage id="homeheader.support" />
+              </div>
+              <div className="flag">
+                <div
+                  className={
+                    language === LANGUAGES.VI
+                      ? "language-vi active"
+                      : "language-vi"
+                  }
+                >
+                  <span onClick={() => this.changeLanguage(LANGUAGES.VI)}>
+                    VN
+                  </span>
+                </div>
+                <div
+                  className={
+                    language === LANGUAGES.EN
+                      ? "language-en active"
+                      : "language-en"
+                  }
+                >
+                  <span onClick={() => this.changeLanguage(LANGUAGES.EN)}>
+                    EN
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {this.props.isShowBanner === true && (
+          <div className="home-header-banner">
+            <div className="content-up">
+              <div className="title-1">
+                <FormattedMessage id="banner.title1" />
+              </div>
+              <div className="title-2">
+                <FormattedMessage id="banner.title2" />
+              </div>
+              <SearchDoctorByName />
+            </div>
+            <div className="content-down">
+              <div className="options">
+                <div className="option-child">
+                  <div className="icon-child">
+                    <img src={khamchuyenkhoa} />
+                  </div>
+                  <div className="text-child">
+                    <b>
+                      <FormattedMessage id="banner.bannerp1" />
+                    </b>
+                  </div>
+                </div>
+                <div className="option-child">
+                  <div className="icon-child">
+                    <img src={khamtuxa} />
+                  </div>
+                  <div className="text-child">
+                    <b>
+                      <FormattedMessage id="banner.bannerp2" />
+                    </b>
+                  </div>
+                </div>
+                <div className="option-child">
+                  <div className="icon-child">
+                    <img src={khamtongquat} />
+                  </div>
+                  <div className="text-child">
+                    <b>
+                      <FormattedMessage id="banner.bannerp3" />
+                    </b>
+                  </div>
+                </div>
+                <div className="option-child">
+                  <div className="icon-child">
+                    <img src={dichvuxetnghiem} />
+                  </div>
+                  <div className="text-child">
+                    <b>
+                      <FormattedMessage id="banner.bannerp4" />
+                    </b>
+                  </div>
+                </div>
+                <div className="option-child">
+                  <div className="icon-child">
+                    <img src={suckhoetinhthan} />
+                  </div>
+                  <div className="text-child">
+                    <b>
+                      <FormattedMessage id="banner.bannerp5" />
+                    </b>
+                  </div>
+                </div>
+                <div className="option-child">
+                  <div className="icon-child">
+                    <img src={khamnhakhoa} />
+                  </div>
+                  <div className="text-child">
+                    <b>
+                      <FormattedMessage id="banner.bannerp6" />
+                    </b>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </React.Fragment>
+    );
+  }
 }
 
-const mapStateToProps = state => {
-    return {
-        isLoggedIn: state.user.isLoggedIn,
-        language: state.app.language,
-
-    };
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.user.isLoggedIn,
+    language: state.app.language,
+  };
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-        changeLanguageAppRedux: (language) => dispatch(changeLanguageApp(language))
-    };
+const mapDispatchToProps = (dispatch) => {
+  return {
+    changeLanguageAppRedux: (language) => dispatch(changeLanguageApp(language)),
+  };
 };
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(HomeHeader));
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(HomeHeader)
+);
